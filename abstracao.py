3f8e28a5a6b381b717414259b0c93aa34e0fd5ff
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-18 00:23:53 -0300

    Add files via upload

---
+++ abstracao.py
@@ -0,0 +1,9 @@
+"Calcular a nota média dos alunos"
+aluno1 = 7.0
+aluno2 = 5.0
+aluno3 = 6.0
+aluno4 = 2.0
+qtd_alunos = 4
+soma_nota_alunos = aluno1 + aluno2 + aluno3 + aluno4
+media = soma_nota_alunos / qtd_alunos
+print("A média das notas é: ", media)
