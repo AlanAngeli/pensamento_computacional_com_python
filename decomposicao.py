9bcdbb621ba942925cc4737223ffd8b976521635
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-18 00:33:04 -0300

    Add files via upload

---
+++ decomposicao.py
@@ -0,0 +1,10 @@
+descontos = 1200.00
+salario_fixo = 16000.00
+vendas_mes = 50
+valor_comissao = 200.00
+valor_total_vendas = vendas_mes * valor_comissao
+
+valor_salario_liquido = (salario_fixo + valor_total_vendas) - descontos
+
+print(valor_salario_liquido)
+
